bcdd862d50f843ce80c78f98a7fa2fa4777b5a21
Sarah <eaaskh@students.eaaa.dk>
2019-04-29 13:13:34 +0200

    Alle filer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Når der klikkes på hamburger-ikonet åbnes/lukkes navItems, der er selve menuen - afhængigt af om den er synlig eller ej  */
 document.getElementById("icon").addEventListener("click", navFunction);
 
 document.getElementById("navItems").addEventListener("click", navFunction)
@@ -5,24 +6,130 @@ document.getElementById("navItems").addEventListener("click", navFunction)
 function navFunction() {
 	var x = document.getElementById("navItems");
 	var z = document.getElementById("nav");
-	if (x.style.display === "block") {
+	var main = document.querySelector("main");
+	var footer = document.querySelector("footer");
+	if (x.style.display === "block" && $(window).width() <=1024) {
 		x.style.display = "none";
 		z.style.height = "7vh";
-		s.style.height = "100%";
-	} else {
+		main.style.filter = "blur(0px)";
+		footer.style.filter = "blur(0px)";
+	} else if ($(window).width() <=1024) {
 		x.style.display = "block";
 		z.style.height = "auto";
+		main.style.filter = "blur(5px)";
+		footer.style.filter = "blur(5px)";
+	} else {
+		x.style.display = "flex";
+		z.style.height = "10vh";
 	}
 };
+/* KILDE til window width: https://stackoverflow.com/questions/7405213/jquery-if-statement-based-on-screen-size */
 
-$( function() {
+/* Når der klikkes et eller andet sted i main området, lukker navigationsbaren */
+document.getElementsByTagName("main")[0].addEventListener('click', () => {
+	var x = document.getElementById("navItems");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	}
+});
+
+/* Når der klikkes på et navItem, er der en mere glidende scroll til det specifikke sted */
+$('a[href*="#"]').on('click', function(e){
+    e.preventDefault()
+  
+    $(' html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      },
+      350,
+	  'linear'
+    )
+  });
+/* KILDE: https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/ */
+
+// Split slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("pFelt");
+/*   var dots = document.getElementsByClassName("dot"); */
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+/*   for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  } */
+  slides[slideIndex-1].style.display = "flex";   
+/*   dots[slideIndex-1].className += " active"; */
+}
+/* KILDE: https://www.w3schools.com/howto/howto_js_slideshow.asp */
+
+
+/* Service artiklen, hentet fra Jquery UI, tilpasset til min egen kode */
+$(function () {
 	var icons = {
 		header: "fas fa-plus",
 		activeHeader: "fas fa-minus"
 	};
-	$( "#sFelter" ).accordion({
+	$("#sFelter").accordion({
 		icons: icons,
 		collapsible: true
 	});
-} );
+});
 /* Jquery UI KILDE: https://jqueryui.com/accordion/#custom-icons */
+
+/* KONTAKT Firebase opsætning og alt med kontaktformularen*/
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBt-9q0PcxT6O4kcO-qKqoolMTkqRU1Y_I",
+    authDomain: "mb-games.firebaseapp.com",
+    databaseURL: "https://mb-games.firebaseio.com",
+    projectId: "mb-games",
+    storageBucket: "",
+    messagingSenderId: "397270763884"
+  };
+  firebase.initializeApp(config);
+
+  //Data Indsættelse
+firebase.database().ref('kontaktBesked').on('value', snapshots => {
+    var kInfo = [];
+    snapshots.forEach(snapshot => {
+        let key = snapshot.key; // saves the key value in the variable key
+        let bBesked = snapshot.val(); // saves the data in the variable bEvent
+        bBesked.key = key; // addes the key to my bEvent object
+        kInfo.push(bBesked);
+    });
+});
+
+/* SEND - push informationen fra kontaktformular elementerne til firebasen. Der bliver ikke hentet information tilbage, da indholdet kun skal kunne vises i firebase.  */
+document.getElementById("send").addEventListener('click', () => {
+    var navn = document.querySelector("#kontaktFormular [name=navn]").value;
+    var email = document.querySelector("#kontaktFormular [name=email]").value;
+    var tlf = document.querySelector("#kontaktFormular [name=tlf]").value;
+    var besked = document.querySelector("#kontaktFormular [name=besked]").value;
+
+    if (navn && email && besked) {
+         firebase.database().ref('kontaktBesked').push({
+            navn: navn,
+            email: email,
+            tlf: tlf,
+            besked: besked
+        });
+		document.getElementById("kontaktFormular").reset();
+		document.getElementById("alert").style.display = "block";
+		setTimeout(function(){
+			document.getElementById("alert").style.display = "none";
+		},5000);
+
+    } else {
+        alert("Venligst indtast dit navn, din email og din besked");
+    }
+});
